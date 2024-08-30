c03c6fe5f6970334abe72106127ab0f5a2e67cf2
Yohan <yohan@uchicago.edu>
2024-08-30 16:13:04 -0500

    Implemented Conv2d, MaxPooling2d, and Dense Layers for definition Added early stopping during training

--- train_model.py
+++ train_model.py
@@ -15,9 +15,13 @@ x_test = x_test.reshape(-1, 28, 28, 1)
 
 # defining model
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
